434987ca3af0a9116dffcc5177d15db1e7bc6ccc
ks-cap <capricorn090114@gmail.com>
2019-08-11 16:05:55 +0900

    Initial Commit

---
+++ GitHubRepository/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHubRepository
+//
+//  Created by 佐藤 賢 on 2019/08/11.
+//  Copyright © 2019 佐藤 賢. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
