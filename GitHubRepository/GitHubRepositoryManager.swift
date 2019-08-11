a4100e13357bc5aa15701f864fa88a0261d86d7e
ks-cap <capricorn090114@gmail.com>
2019-08-11 18:26:32 +0900

    Modelクラス実装

---
+++ GitHubRepository/GitHubRepositoryManager.swift
@@ -0,0 +1,28 @@
+//
+//  GitHubRepositoryManager.swift
+//  GitHubRepository
+//
+//  Created by 佐藤 賢 on 2019/08/11.
+//  Copyright © 2019 佐藤 賢. All rights reserved.
+//
+
+class GitHubRepositoryManager {
+    private let client: GitHubAPIClient
+    private var repos: [GitHubRepository]?
+    
+    var majorRepositories: [GitHubRepository] {
+        guard let repositories = self.repos else { return [] }
+        return repositories.filter { $0.star >= 10 }
+    }
+
+    init() {
+        self.client = GitHubAPIClient()
+    }
+
+    func load(user: String, completion: @escaping () -> Void) {
+        self.client.fetchRepositories(user: user) { repositories in
+            self.repos = repositories
+            completion()
+        }
+    }
+}
